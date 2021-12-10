d6ddd29ecbc2ec0561aa3bb75bd49d5dc8b9f1b8
Maciej Kawęcki <skimiskim12@gmail.com>
2021-12-10 23:15:20 +0100

    Add atuo migration, config and enitites new

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
+
+namespace API.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(StoreContext context, IProductRepository productRepository)
+        {
+            _context = context;
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _productRepository.GetProductsAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _productRepository.GetProductByIdAsync(id);
+        }
+    }
+}
