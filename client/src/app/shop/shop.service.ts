bd95e992c6ed07c3b583b98bcbfd1c9e18e248fd
Maciej Kawęcki <skimiskim12@gmail.com>
2021-12-21 16:34:20 +0100

    Add routing and product view

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -4,8 +4,9 @@ import { Observable } from 'rxjs';
 import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IProductType } from '../shared/models/productType';
-import { map } from 'rxjs/operators';
+import { map, retry } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -42,6 +43,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
